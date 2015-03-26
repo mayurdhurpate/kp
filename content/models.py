665f5120147af87704515469a88b0d625ff747cf
Mayank Rajani <mkrjn99@gmail.com>
2015-03-26 19:17:15 +0530

    Added some barebones for models.py

--- content/models.py
+++ content/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category:
+    title
+
+class Content:
+    tags
+    title
+    date
+    abstract
+    image
+    category
+    links
+
+class User:
+    username
+    profession
+    contents
+
